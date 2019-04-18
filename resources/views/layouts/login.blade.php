0342d8ca0ef1ba149255e1494144dd6a59541870
shokoylele <iamlele17@gmail.com>
2019-04-18 15:41:24 +0800

    theming

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>iRESERVE-Login</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{URL::asset('css/adminlte_css/font-awesome.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('css/adminlte_css/ionicons.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('css/adminlte_css/dataTables.bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('css/adminlte_css/AdminLTE.min.css')}}" >
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  @yield('scripts')
+</head>
+
+<body class="hold-transition login-page" style="overflow-y: hidden;">
+    <div class="login-box">
+        @yield('content')
+    </div>
+</body>
+</html>
