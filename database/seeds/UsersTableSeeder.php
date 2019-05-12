239e469a484722ec6650d0d270421eb14f12445f
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2019-05-12 14:42:36 +0800

    edited users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -26,7 +26,7 @@ class UsersTableSeeder extends Seeder
                 'user_id' => '201701054',
                 'name' => 'Lenard Valdez',
                 'email' => '201701054@iacademy.edu.ph',
-                'password' => bcrypt('userpassword'),
+                'password' => bcrypt('studentpassword'),
                 'roles' => '1', //0=admin, 1=user, 2=security
                 'isActive' => true,
                 'created_at' => '2019/05/03 11:01:23',
@@ -36,7 +36,7 @@ class UsersTableSeeder extends Seeder
                 'user_id' => '201701026',
                 'name' => 'Amiel Roseller II Saballo',
                 'email' => '201701026@iacademy.edu.ph',
-                'password' => bcrypt('userpassword2'),
+                'password' => bcrypt('studentpassword'),
                 'roles' => '1', //0=admin, 1=user, 2=security
                 'isActive' => true,
                 'created_at' => '2019/05/03 11:01:00',
@@ -46,50 +46,10 @@ class UsersTableSeeder extends Seeder
                 'user_id' => '201701065',
                 'name' => 'Janzon Jon Victorio',
                 'email' => '201701065@iacademy.edu.ph',
-                'password' => bcrypt('userpassword3'),
+                'password' => bcrypt('studentpassword'),
                 'roles' => '1', //0=admin, 1=user, 2=security
                 'isActive' => true,
                 'created_at' => '2019/05/03 11:02:00',
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'user_id' => 'marikit.valmadrid',
-                'name' => 'Marikit Valmadrid',
-                'email' => 'marikit.valmadrid@iacademy.edu.ph',
-                'password' => bcrypt('userpassword4'),
-                'roles' => '1', //0=admin, 1=user, 2=security
-                'isActive' => true,
-                'created_at' => '2019/05/03 11:03:00',
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'user_id' => '201701065',
-                'name' => 'Janzon Victorio',
-                'email' => '201701065@iacademy.edu.ph',
-                'password' => bcrypt('studentpassword'),
-                'roles' => '1', //0=admin, 1=user, 2=security
-                'isActive' => true,
-                'created_at' => '2019/05/03 11:00:23',
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'user_id' => '201701026',
-                'name' => 'Amiel Saballo',
-                'email' => '201701026@iacademy.edu.ph',
-                'password' => bcrypt('studentpassword'),
-                'roles' => '1', //0=admin, 1=user, 2=security
-                'isActive' => true,
-                'created_at' => '2019/05/03 11:00:23',
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'user_id' => '201702012',
-                'name' => 'Nicole Bilon',
-                'email' => '201702012@iacademy.edu.ph',
-                'password' => bcrypt('studentpassword'),
-                'roles' => '1', //0=admin, 1=user, 2=security
-                'isActive' => true,
-                'created_at' => '2019/05/03 11:00:23',
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
