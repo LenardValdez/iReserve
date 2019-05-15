16263ba65697b0788e74e25e75f7b41ec12b425f
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2019-05-16 04:14:14 +0800

    made request confirmation alerts dismissible

--- resources/views/pages/requests.blade.php
+++ resources/views/pages/requests.blade.php
@@ -61,13 +61,19 @@
             <div class="col-md-12">
               
               @if(session('approvedAlert'))
-              <div class="alert alert-success">
+              <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
                 {{ session('approvedAlert') }}
               </div>
               @endif
 
               @if(session('rejectedAlert'))
-              <div class="alert alert-danger">
+              <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
                 {{ session('rejectedAlert') }}
               </div>
               @endif
