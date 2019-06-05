26235141152fcd6b9cfe868a235d78759d248385
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2019-06-05 14:24:38 +0800

    fixed room dropdown (working)

--- resources/views/pages/reservation.blade.php
+++ resources/views/pages/reservation.blade.php
@@ -21,9 +21,44 @@
 @endsection
 
 @section('script')
-<script src="js/select2-cascade.js"></script>
-
 <script>
+var Select2Cascade = ( function(window, $) {
+
+    function Select2Cascade(parent, child, url, select2Options) {
+        var afterActions = [];
+        var options = select2Options || {};
+
+        // Register functions to be called after cascading data loading done
+        this.then = function(callback) {
+            afterActions.push(callback);
+            return this;
+        };
+
+        parent.select2(select2Options).on("change", function (e) {
+
+            child.prop("disabled", true);
+            var _this = this;
+            
+            $.getJSON(url.replace(':parentId:', $(this).val()), function(items) {
+                var newOptions = '<option value="" disabled>Select room</option>';
+                for(var id in items) {
+                    newOptions += '<option value="'+ id +'">'+ items[id] +'</option>';
+                }
+
+                child.select2('destroy').html(newOptions).prop("disabled", false)
+                    .select2(options);
+                
+                afterActions.forEach(function (callback) {
+                    callback(parent, child, items);
+                });
+            });
+        });
+    }
+
+    return Select2Cascade;
+
+})( window, $);
+
 $(function () {
     $('.select2').select2({
         tags: true,
