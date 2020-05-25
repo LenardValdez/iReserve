fae3442156f87bab20062404bcafe439da0fb8e9
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2020-05-25 12:54:21 +0800

    added new middleware to Kernel.php

--- config/app.php
+++ config/app.php
@@ -199,7 +199,7 @@ return [
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Carbon' => 'Carbon\Carbon',
+        'Carbon' => Carbon\Carbon::class,
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
